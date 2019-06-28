12770b9f08a16ec2292287b32381702b1a3b37fd
ChrisKeefe <crk239@nau.edu>
2019-06-27 17:50:10 -0700

    refactors test data for readability, diagnostic quality, brevity

--- q2_diversity_lib/tests/test_alpha.py
+++ q2_diversity_lib/tests/test_alpha.py
@@ -25,47 +25,39 @@ class AlphaTests(TestPluginBase):
 
     def setUp(self):
         super().setUp()
-        self.input_table = biom.Table(np.array([[0, 1, 3], [1, 1, 2]]),
-                                      ['O1', 'O2'],
-                                      ['S1', 'S2', 'S3'])
+        self.input_table = biom.Table(np.array([[1, 0, .5, 999, 1],
+                                                [0, 1, 2, 0, 1],
+                                                [0, 0, 0, 1, 1]]),
+                                      ['A', 'B', 'C'],
+                                      ['S1', 'S2', 'S3', 'S4', 'S5'])
+        self.input_tree = skbio.TreeNode.read(io.StringIO(
+                '((A:0.3, B:0.50):0.2, C:100)root;'))
+        self.faith_pd_expected = pd.Series({'S1': 0.5, 'S2': 0.7, 'S3': 1.0,
+                                            'S4': 100.5, 'S5': 101},
+                                           name='faith_pd')
 
     def test_faith_pd(self):
-        tree = skbio.TreeNode.read(io.StringIO(
-            '((O1:0.25, O2:0.50):0.25, O3:0.75)root;'))
-        actual = faith_pd(table=self.input_table, phylogeny=tree)
-        # expected computed with skbio.diversity.alpha_diversity
-        expected = pd.Series({'S1': 0.75, 'S2': 1.0, 'S3': 1.0},
-                             name='faith_pd')
-        pdt.assert_series_equal(actual, expected)
+        actual = faith_pd(table=self.input_table, phylogeny=self.input_tree)
+        pdt.assert_series_equal(actual, self.faith_pd_expected)
 
     def test_faith_pd_error_rewriting(self):
         tree = skbio.TreeNode.read(io.StringIO(
-            '((O1:0.25):0.25, O3:0.75)root;'))
-        # Verify through regex that there is a ``feature_ids`` substring
-        # followed by a ``phylogeny``
+            '((A:0.3):0.2, C:100)root;'))
         with self.assertRaisesRegex(skbio.tree.MissingNodeError,
                                     'feature_ids.*phylogeny'):
             faith_pd(table=self.input_table, phylogeny=tree)
 
-
-# TODO: adjust test tree to ensure our results are diagnostic
-    def test_accepted_types_consistent_behavior(self):
-        tree = skbio.TreeNode.read(io.StringIO(
-            '((O1:0.25, O2:0.50):0.25, O3:0.75)root;'))
+    def test_all_accepted_types_have_consistent_behavior(self):
         freq_table = self.input_table
         rel_freq_table = copy.deepcopy(self.input_table).norm(axis='sample',
                                                               inplace=False)
         p_a_table = copy.deepcopy(self.input_table).pa()
         accepted_tables = [freq_table, rel_freq_table, p_a_table]
-        expected = pd.Series({'S1': 0.75, 'S2': 1.0, 'S3': 1.0},
-                             name='faith_pd')
         for table in accepted_tables:
-            actual = faith_pd(table=table, phylogeny=tree)
-            pdt.assert_series_equal(actual, expected)
+            actual = faith_pd(table=table, phylogeny=self.input_tree)
+            pdt.assert_series_equal(actual, self.faith_pd_expected)
 
     def test_alpha_phylogenetic_empty_table(self):
         empty_table = biom.Table(np.array([]), [], [])
-        tree = skbio.TreeNode.read(io.StringIO(
-            '((O1:0.25):0.25, O3:0.75)root;'))
         with self.assertRaisesRegex(ValueError, "empty"):
-            faith_pd(table=empty_table, phylogeny=tree)
+            faith_pd(table=empty_table, phylogeny=self.input_tree)
