ba7ff865da124f95ce197e575a1070638397c1a8
ChrisKeefe <crk239@nau.edu>
2019-07-12 15:52:48 -0700

    LINT: whitespace

--- q2_diversity_lib/_alpha/_method.py
+++ q2_diversity_lib/_alpha/_method.py
@@ -35,7 +35,7 @@ def faith_pd(table: biom.Table, phylogeny: skbio.TreeNode) -> pd.Series:
     return result
 
 
-def pielou_evenness(table: biom.Table, drop_nans: bool=False) -> pd.Series:
+def pielou_evenness(table: biom.Table, drop_nans: bool = False) -> pd.Series:
     if table.is_empty():
         raise ValueError("The provided table object is empty")
     counts = table.matrix_data.toarray().T
