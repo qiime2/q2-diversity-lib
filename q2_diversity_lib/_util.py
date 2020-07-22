0a5dabb754d13c6376b81554ee4eeed119d84fb7
Chris Keefe <crk239@nau.edu>
2020-07-22 14:01:34 -0700

    SQUASH: explicitly labels arguments to np.sum and np.delete axis params

--- q2_diversity_lib/_util.py
+++ q2_diversity_lib/_util.py
@@ -19,10 +19,10 @@ from q2_types.feature_table import BIOMV210Format
 def _drop_undefined_samples(counts: np.ndarray, sample_ids: np.ndarray,
                             minimum_nonzero_elements: int) -> (np.ndarray,
                                                                np.ndarray):
-    nonzero_elements_per_sample = (counts != 0).sum(1)
+    nonzero_elements_per_sample = (counts != 0).sum(axis=1)
     fancy_index = np.where(
             nonzero_elements_per_sample < minimum_nonzero_elements)
-    filtered_counts = np.delete(counts, fancy_index, 0)
+    filtered_counts = np.delete(counts, fancy_index, axis=0)
     filtered_sample_ids = np.delete(sample_ids, fancy_index)
     return (filtered_counts, filtered_sample_ids)
 
