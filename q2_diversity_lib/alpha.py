9439531c78c07e554c54f1dacdedb1f96726b46d
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-04-02 10:23:03 -0700

    Be sparse for observed and peilou's

--- q2_diversity_lib/alpha.py
+++ q2_diversity_lib/alpha.py
@@ -10,6 +10,7 @@ import pandas as pd
 import skbio.diversity
 import biom
 import unifrac
+import numpy as np
 
 from q2_types.feature_table import BIOMV210Format
 from q2_types.tree import NewickFormat
@@ -55,27 +56,36 @@ def faith_pd(table: BIOMV210Format, phylogeny: NewickFormat) -> pd.Series:
 @_disallow_empty_tables
 def observed_features(table: biom.Table) -> pd.Series:
     presence_absence_table = table.pa(inplace=False)
-    counts = presence_absence_table.matrix_data.toarray().astype(int).T
-    sample_ids = presence_absence_table.ids(axis='sample')
-    result = skbio.diversity.alpha_diversity(metric='observed_otus',
-                                             counts=counts, ids=sample_ids)
-    result.name = 'observed_features'
-    return result
+    results = []
+    for v in presence_absence_table.iter_data(dense=True):
+        result = skbio.diversity.alpha_diversity(metric='observed_otus',
+                                                 counts=v.astype(int),
+                                                 ids=['placeholder', ])
+        results.append(result.iloc[0])
+    results = pd.Series(results, index=table.ids(), name='observed_features')
+    return results
 
 
 @_disallow_empty_tables
 def pielou_evenness(table: biom.Table,
                     drop_undefined_samples: bool = False) -> pd.Series:
-    counts = table.matrix_data.toarray().T
-    sample_ids = table.ids(axis='sample')
     if drop_undefined_samples:
-        counts, sample_ids = _drop_undefined_samples(
-                counts, sample_ids, minimum_nonzero_elements=2)
+        def transform_(v, i, m):
+            if (v > 0).sum() < 2:
+                return np.zeros(len(v))
+            else:
+                return v
 
-    result = skbio.diversity.alpha_diversity(metric='pielou_e', counts=counts,
-                                             ids=sample_ids)
-    result.name = 'pielou_evenness'
-    return result
+        table = table.transform(transform_, inplace=False).remove_empty()
+
+    results = []
+    for v, i, m in table.iter(dense=True):
+        result = skbio.diversity.alpha_diversity(metric='pielou_e',
+                                                 counts=v,
+                                                 ids=['placeholder', ])
+        results.append(result.iloc[0])
+    results = pd.Series(results, index=table.ids(), name='pielou_evenness')
+    return results
 
 
 @_disallow_empty_tables
