b6abce92d31348100e7680a3c8da947c83157b98
Chris Keefe <39198770+ChrisKeefe@users.noreply.github.com>
2022-06-22 00:19:28 +0200

    new: usage examples (#39)

---
+++ q2_diversity_lib/tests/test_examples.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2018-2022, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from qiime2.plugin.testing import TestPluginBase
+
+
+class UsageExampleTests(TestPluginBase):
+    package = 'q2_diversity_lib.tests'
+
+    def test_usage_examples(self):
+        self.execute_examples()
