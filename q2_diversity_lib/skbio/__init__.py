a162614ab73c1a76557ad3ded4e866ee74d1a58c
Liz Gehret <elizabeth.gehret@gmail.com>
2024-05-07 16:41:12 -0700

    init

---
+++ q2_diversity_lib/skbio/__init__.py
@@ -0,0 +1,5 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
